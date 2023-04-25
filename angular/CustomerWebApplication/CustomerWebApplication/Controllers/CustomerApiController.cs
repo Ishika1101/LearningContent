acd39c07204690990e0a085683d14a5607a5ad06
ishika <ishikagangwal16@gmail.com>
2023-04-26 00:04:22 +0530

    add existing file

---
+++ angular/CustomerWebApplication/CustomerWebApplication/Controllers/CustomerApiController.cs
@@ -0,0 +1,48 @@
+﻿using CustomerWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerApiController : ControllerBase
+    {
+        static List<Customer> list = new List<Customer>();
+        // GET: api/<CustomerApiController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CustomerApiController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomerApiController>
+        [HttpPost]
+        
+        public List<Customer> Post([FromBody] Customer cust)
+        {
+            list.Add(cust);
+            return list.ToList();
+        }
+
+        // PUT api/<CustomerApiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ angular/CustomerWebApplication/CustomerWebApplication/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerWebApplication.Models
+{
+    public class Customer
+    {
+        public int CustomerCode { get; set; }
+        public string CustomerName { get; set; }
+        public decimal CustomerAmount { get; set; }
+    }
+}
